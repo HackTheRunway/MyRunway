594b0d566cc66960e7b1ebbcec3f692431d600e9
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-07-16 19:00:07 +0800

    Create new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { WardrobecamComponent } from './wardrobecam/wardrobecam.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    WardrobecamComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/wardrobecam/wardrobecam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wardrobecam',
+  templateUrl: './wardrobecam.component.html',
+  styleUrls: ['./wardrobecam.component.scss']
+})
+export class WardrobecamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
