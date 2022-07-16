d44f4275c2aded5fa22f36d6a2c9006d314d66b0
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-07-16 19:16:10 +0800

    Set up basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { WardrobecamComponent } from './wardrobecam/wardrobecam.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'wardrobecam', component: WardrobecamComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
