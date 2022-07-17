f723b8ba66d3c6def70725033c1216e08afceba0
e0425705 <puahxiuwen@gmail.com>
2022-07-17 14:56:49 +0800

    update

--- src/app/view-wardrobe/view-wardrobe.component.ts
+++ src/app/view-wardrobe/view-wardrobe.component.ts
@@ -17,6 +17,13 @@ export class ViewWardrobeComponent implements OnInit {
   public wearableTitle = "";
   public isFieldsValidated = false;
 
+  sampleData:any;
+  textInput = '';
+  displayValue: string = '';
+  data: string = '';
+
+  msg:string = '';
+
   constructor(service: DataStorageService) { 
     this.service = service;
     this.wearables = service.getWearables();
@@ -34,4 +41,44 @@ export class ViewWardrobeComponent implements OnInit {
     this.isFieldsValidated= this.fieldsValidated();
   }
 
+  // ngOnInit() {
+  //   this._jsonService.getData().subscribe((data) => {
+  //     this.sampleData = data;
+  //   });
+  // }
+
+  onClick(message: string) {
+    //this.displayValue = this.textInput;
+    //this.msg='Button is Clicked';
+    
+    message = message.toLowerCase();
+
+    switch(message) {
+      case "top":
+        this.msg = "The category selected is Top.";
+        break;
+      case "bottom":
+        this.msg = "The category selected is Bottom.";
+        break;
+      case "accessory":
+        this.msg = "The category selected is Accessory.";
+        break;
+      case "hat":
+        this.msg = "The category selected is Hat.";
+        break;
+      case "shoes":
+        this.msg = "The category selected is Shoes.";
+        break;
+      case "socks":
+        this.msg = "The category selected is Socks.";
+        break;
+      default:
+        this.msg = "Sorry I do not know this category. Please select from the following available categories: 1.Top 2.Bottom 3.Accessory 4.Hat 5.Shoes 6.Socks"
+        break;
+    }
+
+    // process to display images
+
+    return this.msg;
+  }
 }
